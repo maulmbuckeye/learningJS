a874485848d1935ecaa753c51469841c7f91bc1d
Mike Ulm <mike.ulm@nokia.com>
2016-06-22 11:01:14 -0500

    add generic curry function schonfinkelize

--- js/curry.js
+++ js/curry.js
@@ -19,3 +19,14 @@ typeof add(5);   // "function"
 add(3)(4);    // 7
 var add2000 = add(2000);
 add2000(10); // 2010
+
+function schonfinkelize(fn) {
+    // borrow slice from Array since arguements is not a real array in JavaScript
+	var slice = Array.prototype.slice;
+	var stored_args = slice.call(arguments, 1);
+	return function () {
+		var new_args = slice.call(arguments);
+		var args = stored_args.concat(new_args);
+		return fn.apply(null, args);
+	};
+}
